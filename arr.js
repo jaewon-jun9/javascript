823cda5e4bfb5b2be31d048e2cdb57d595fe4f6f
Jung Jaewon <58683003+jaewon-jun9@users.noreply.github.com>
2020-11-15 23:43:39 +0900

    Create arr.js

---
+++ arr.js
@@ -0,0 +1,11 @@
+const color = ['white', 'red', 'black', 'yellow'];
+
+// 배열의 길이 체크 (length)
+document.write(color.length);
+document.write('<br />');
+
+// 배열의 길이 set
+color.length = 2;
+document.write(color.length);
+document.write('<br />');
+document.write(color);
