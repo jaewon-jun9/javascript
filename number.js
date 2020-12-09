e8c222c0673182ace7fbf689420f3d3ef5f33d20
Jung Jaewon <58683003+jaewon-jun9@users.noreply.github.com>
2020-12-09 23:50:59 +0900

    Update number.js

--- number.js
+++ number.js
@@ -1,10 +1,10 @@
-var str1 = 50 + " meters";  // 50 meters
-var str2 = 50 + "";
+let str1 = 50 + " meters";  // 50 meters
+let str2 = 50 + "";
 console.log(str1);           // NaN
 console.log(str2); 
-var number1 = 500;
-var string1 = number1.toString();
-console.log(number1.length);    // undefined
+let number = 500;
+let string1 = number.toString();
+console.log(number.length);    // undefined
 console.log(string1.length);    // 3
 console.log("50"*"5");  // 250
 console.log(+"50");     // 50
@@ -16,18 +16,19 @@ console.log(isFinite(-1/0));    // false
 console.log(isFinite("a"));     // false
 console.log(isFinite(NaN));     // false
 console.log(isFinite(1));       // true
-var number1 = 1;            
-var number2 = 0;            
-var number3 = 999999999;    
-var number4 = 0xff;
-
-var number6 = 3.14159265;
-var number7 = 5e5;      // 과학적 표기법
-var number8 = 5E-5;     // 과학적 표기법
+let number1 = 1;            
+let number2 = 0;            
+let number3 = 999999999;    
+let number4 = 0xff;
+let number5 = 010;
+let number6 = 3.14159265;
+let number7 = 5e5;      // 과학적 표기법
+let number8 = 5E-5;     // 과학적 표기법
 console.log("number1 = " + number1);
 console.log("number2 = " + number2);
 console.log("number3 = " + number3);
 console.log("number4 = " + number4);
+console.log("number5 = " + number5);
 console.log("number6 = " + number6);
 console.log("number7 = " + number7);
 console.log("number8 = " + number8);
